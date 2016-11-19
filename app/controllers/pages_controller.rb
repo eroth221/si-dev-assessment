7984828ad332129ba71627e3a33ef42a0d5da566
Evan Rothstein <evanhrothstein@gmail.com>
2016-11-19 18:59:55 +0000

    Finishing Task 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   end
 
   def sms
+    if(params.has_key?(:message) && params.has_key?(:number))
+      @message = params[:message]
+      @number = params[:number]
+    end
   end
 end
